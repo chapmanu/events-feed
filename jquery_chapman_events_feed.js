379ee3b921e510a05a1dd806023a251d268848fa
James Kerr <jkerr838@gmail.com>
2015-11-06 09:00:16 -0800

    Fixed something

--- jquery_chapman_events_feed.js
+++ jquery_chapman_events_feed.js
@@ -13,8 +13,9 @@
    */
   ChapmanEventsFeed = function(options) {
     if (!options || !options.url) throw "ChapmanEventsFeed needs to have a url passed into the options object."
-    this.url = this.parseUrl(options.url);
-  }
+    this.$element = options.$element;
+    this.url      = this.parseUrl(options.url);
+  };
 
 
   /**
@@ -25,24 +26,54 @@
     parser.href = url;
     parser.pathname += 'feed';
     return parser.href;
-  }
+  };
 
 
   /**
    * Makes the ajax request to our endpoint.  This expects an html response.
    */
   ChapmanEventsFeed.prototype.fetchEvents = function(callback) {
-    $.get(this.url, callback);
-  }
+    $.ajax({
+      method:  'GET',
+      url:     this.url,
+      success: this.onSuccess.bind(this),
+      error:   this.onError.bind(this)
+    });
+  };
+
+  /**
+   * The callback for when ajax was a success
+   */
+  ChapmanEventsFeed.prototype.onSuccess = function(data) {
+    this.$element.html(data);
+  };
+
+  /**
+   * The bacllback for when ajax encountered an error;
+   */
+  ChapmanEventsFeed.prototype.onError = function(message) {
+    this.$element.html("<p>There was a problem loading events.</p>")
+  };
 
 
   /**
    * The jQuery extention so that we can use this easily.
    */
   $.fn.chapmanEventsFeed = function(options) {
-    var self = this;
-    new ChapmanEventsFeed(options).fetchEvents(function(data) { self.html(data); });
+    options.$element = this;
+    new ChapmanEventsFeed(options).fetchEvents();
     return this;
   };
 
 })( jQuery );
+
+
+
+/**
+ * Runs on Document ready
+ */
+$(document).ready(function() {
+  $('[data-chapman-events-feed]').each(function() {
+    $(this).chapmanEventsFeed({url: $(this).data('chapman-events-feed')});
+  });
+});
